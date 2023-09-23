a8477458fe6fbd385b59c147761fc46b3a0058bd
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-09-24 02:42:49 +0300

    Create p6-arc.cpp

---
+++ p6-arc.cpp
@@ -0,0 +1,9 @@
+	#include<graphics.h>
+	int main()
+	{
+	    initwindow(800,800);
+	    //arc(int x, int y, int stangle, int endangle, int radius);
+	    arc(100, 100, 0, 135, 50);
+   		
+	    getch();	
+	}
