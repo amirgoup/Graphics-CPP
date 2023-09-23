467fd6ff94fef111e827f214fb304d93f69415e9
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-09-24 02:59:56 +0300

    Update p5-ellipse.cpp

--- p5-ellipse.cpp
+++ p5-ellipse.cpp
@@ -3,6 +3,7 @@
 	{
 	    initwindow(800,800);
 	    int x = 320, y = 240, radius;
+	    //ellipse(int x, int y, int stangle, int endangle, int xradius, int yradius)	
 	    ellipse(200, 200, 0, 360, 100, 50);	
 	    getch();	
 	}
