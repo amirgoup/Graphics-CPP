8022e2e4765d142e263e16cc0e967cb362d74faf
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-09-24 02:47:07 +0300

    Create p8-bar3d.cpp

---
+++ p8-bar3d.cpp
@@ -0,0 +1,9 @@
+	#include<graphics.h>
+	int main()
+	{
+	    initwindow(800,800);
+	    //bar3d(int left, int top, int right, int bottom, int depth, int topflag)
+	    bar3d(100, 100, 200, 200, 20, 1);
+   		
+	    getch();	
+	}
