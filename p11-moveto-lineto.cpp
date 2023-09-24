b0ee42d5e67628631af7b6951f793700fa25eb8c
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-09-24 03:38:26 +0300

    Create p11-moveto-lineto.cpp

---
+++ p11-moveto-lineto.cpp
@@ -0,0 +1,12 @@
+	#include<graphics.h>
+	int main()
+	{
+	    initwindow(800,800);
+	    moveto(100, 100);
+   	    lineto(200, 200);   
+   	    getch();
+   	    cleardevice();
+   	    moveto(200, 200);
+   	    lineto(300, 300); 
+	    getch();	
+	}
