083d3a3a80071ec5b432b2e608e0edccf50ef6de
Amir Ibrahim Tahir <amirgoup1@gmail.com>
2023-09-27 14:26:25 +0300

    Create bar-circle.cpp

---
+++ Exercise/bar-circle.cpp
@@ -0,0 +1,11 @@
+#include<graphics.h>
+int main()
+{
+	initwindow(500,500);
+	bar(200, 200, 300, 300);
+	circle(250,250,70.71);
+	line(250,150,250,350);
+	line(150,250,350,250);
+	circle(250,250,100);
+	getch();
+}
